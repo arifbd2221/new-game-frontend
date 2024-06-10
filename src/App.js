9fb43f10460f15db3b3d21f666bfdbef573dd735
arif <mohidul.haque@goama.com>
2024-06-10 23:49:02 +0600

    Fixed repeatitive login

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Leaderboard from './leaderboard/leaderboard';
 import RegisterForm from './registration/register';
 import { PlayGame } from './playGame';
 
+
 function App() {
+  const [isUserRegistered, setIsUserRegistered] = useState(null);
+
+  useEffect(() => {
+    const userRegistered = localStorage.getItem('userid');
+    setIsUserRegistered(userRegistered);
+  }, []);
+
   return (
     <Router>
     <div className="App">
       <Routes>
         <Route path="/leaderboard" element={<Leaderboard />} />
         <Route path="/play" element={<PlayGame />} />
-        <Route path="*" element={<RegisterForm />} />
+        <Route
+            path="/"
+            element={
+              isUserRegistered ? (
+                <Navigate to="/play" />
+              ) : (
+                <RegisterForm />
+              )
+            }
+          />
       </Routes>
     </div>
   </Router>
